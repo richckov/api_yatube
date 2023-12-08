4cab3a5d68f74085501902b86f0aff650792cdaa
Евгений Рычков <fakerichckov@yandex.ru>
2023-12-08 20:27:53 +0500

    first review

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_framework.authtoken',
     'posts.apps.PostsConfig',
     'api.apps.ApiConfig',
     'djoser'
